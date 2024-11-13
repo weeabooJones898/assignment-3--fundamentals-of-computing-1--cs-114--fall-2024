acfc42638c6baf4a346d5156b61bca16615e7a9e
tim <thelastnation49@gmail.com>
2024-11-13 15:30:02 -0500

    made some changes

--- Matrix.java
+++ Matrix.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
 public class Matrix {
+public static void main(String[] args) {
+
+  //User matrix requset
+
+}
+system.out.print('hello')
+
+
+
+
+
+
+
+
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,12 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
-  }
+  Scanner = new Scanner(System.in) ;
+  System.out.println("Resquesting a postive number input please") ;
+Integer size = Scanner
+
+System.out.println("one momnet please proccesing") ;
+}
+
+
 }
