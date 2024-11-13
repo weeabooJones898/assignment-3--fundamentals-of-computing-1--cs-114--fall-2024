a8e55e00f8107a723c4792ab5aedefd454e226d6
tim <thelastnation49@gmail.com>
2024-11-13 15:32:29 -0500

    Update RunMatrix.java

--- RunMatrix.java
+++ RunMatrix.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class RunMatrix {
   public static void main(String[] args) {
   Scanner = new Scanner(System.in) ;
-  System.out.println("Resquesting a postive number input please") ;
-Integer size = Scanner
 
-System.out.println("one momnet please proccesing") ;
+  System.out.println("Resquesting a postive number input please") ;
+    Integer size = Scanner
+
+  System.out.println("one momnet please proccesing") ;
 }
 
-
+that's as far as i got with this code:
 }
